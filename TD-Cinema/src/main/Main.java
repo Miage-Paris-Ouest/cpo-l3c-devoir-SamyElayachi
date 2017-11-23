3a6109c4548be5d2bfd4c93434ace79ccb599775
samy <samy.elayachi8@etu.univ-lorraine.fr>
2017-11-23 17:08:25 +0100

    modif cinema

--- TD-Cinema/src/main/Main.java
+++ TD-Cinema/src/main/Main.java
@@ -1,3 +1,8 @@
+package main;
+
+import modele.SalleCinema;
+
+
 public class Main {
 
     public static void main(String[] args) {
--- TD-Cinema/src/modele/SalleCinema.java
+++ TD-Cinema/src/modele/SalleCinema.java
@@ -1,3 +1,5 @@
+package modele;
+
 public class SalleCinema {
 
     private String film;
